a80d6c8d24559843725c2b9fa50a6f947cb5b259
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-12-20 23:48:20 +0600

    chapter 6 is completed

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,20 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+
+export default async function CardWrapper(totalPaidInvoices:any, totalPendingInvoices:any, numberOfInvoices:any, numberOfCustomers:any) {
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
